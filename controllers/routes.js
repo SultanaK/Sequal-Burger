e347ac66036948cf7232148529199e35420f666d
SultanaK <kr.shampa@yahoo.com>
2018-11-24 14:43:07 -0600

    update

---
+++ controllers/routes.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/', function(req,res){
+    burger.all(function(burger_data){
+        var hbsObject = {
+            burgers: burger_data
+
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.put('/burgers/update',function(req,res){
+    burger.update(req.body.burger_id, function(result){
+        console.log(result);
+        res.redirect('/')
+    });
+
+});
+
+router.post('/burgers/create',function(req,res){
+    burger.create(req.body.burger_name, function(result){
+        res.redirect('/');
+        
+    });
+});
+module.exports = router;
---
+++ database/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured)
+	values('HamBurger', false),
+    ('cheese Burger', false),
+    ('Fish Burger', false);
+    
+    select * from burgers;
